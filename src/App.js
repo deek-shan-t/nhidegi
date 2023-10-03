67faee9bf7f4eab0239147f1336e4c7e19876f9c
deek-shan-t <singhdekson@gmail.com>
2023-10-03 22:50:29 +0530

    thoda bahut kaam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React from "react";
+import Logo from './logo.svg';
 import logo from './logo.svg';
 import './App.css';
+// import Navbar from "./components/head";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
+    <React.StrictMode>
+    <div></div>
+      {/* <BrowserRouter>
+        <Navbar style={{shadow: "none"}}/>
+      </BrowserRouter> */}
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Guys lets take selfie</h1>
+          {/* <h1>Guys lets take selfie</h1> */}
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn Reac
-        </a>
+        </a> */}
       </header>
     </div>
+    </React.StrictMode>  
   );
 }
-
-export default App;
