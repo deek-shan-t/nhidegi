3c60551c6b553f29031eb3ed4e43e645ca7cfb93
deek-shan-t <singhdekson@gmail.com>
2024-01-13 11:47:54 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from "react";
-import Logo from './logo.svg';
 import logo from './logo.svg';
 import './App.css';
-// import Navbar from "./components/head";
+import Navbar from "./components/head";
 import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <React.StrictMode>
     <div></div>
-      {/* <BrowserRouter>
-        <Navbar style={{shadow: "none"}}/>
-      </BrowserRouter> */}
-
-    <div className="App">
+      <BrowserRouter>
+        <Navbar/>
+        <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* <h1>Guys lets take selfie</h1> */}
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
-        </a> */}
       </header>
     </div>
+      </BrowserRouter>
+      
     </React.StrictMode>  
   );
 }
--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,25 +1,10 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { styled, alpha } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
-import Badge from "@mui/material/Badge";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "C:/Volume D/HTML CSS/nhidegi/node_modules/@mui/material/Menu";
-// import SearchIcon from "@mui/icons-material/Search";
-// import AccountCircle from "@mui/icons-material/AccountCircle";
-// import MailIcon from "@mui/icons-material/Mail";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
-// import MoreIcon from "@mui/icons-material/MoreVert";
-import { useAppStore } from "../appStore";
-import { yellow } from "@mui/material/colors";
-// import { Link } from "react-router-dom";
-
 import Logo from "../assets/Screenshot 2023-10-02 161403.png";
 
 const AppBar = styled(
@@ -35,73 +20,23 @@ export default function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="inherit" style={{backdropFilter: "blur(3px)",
-    }} elevation={1}>
+      <AppBar position="fixed" style={{ backgroundColor: "#F5F5F5" }}>
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-            onClick={() => updateOpen(!dopen)}
-          >
-            <MenuIcon />
-          </IconButton> */}
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
+            <img className="head-logo" src={Logo} alt="logo" style={{height: "50px", width: "50px"}}/>
 
-            <Link to="/">
-              <div className="grid grid-cols-4">
-                <div className="mx-auto"><img src={Logo} alt="logo" width="50px" /></div>
-                <div className="col-span-3 my-auto">Student WellBeing Commitee</div>
-              </div>
-              
-              
-              <p></p>
-            </Link>
-
-          </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-          
-          <IconButton
-              size="small"
-              color="inherit"
-            >
+          <div className="head-buttons">
+          <IconButton>
               <Link to="/">Home</Link>
             </IconButton>
-
-          <IconButton
-              size="small"
-              edge="end"
-              aria-label="account of current user"
-            //   aria-controls={menuId}
-              aria-haspopup="true"
-              color="inherit"
-            >
-              Teams
+          <IconButton>
+              <Link to="/">About us</Link>
             </IconButton>
-            <IconButton
-              size="small"
-              color="inherit"
-            >
-              <Link to="https://yourdost.com/">YourDost</Link>
+          <IconButton>
+              <Link to="/">Contact us(don't)</Link>
             </IconButton>
-            
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-haspopup="true"
-              color="inherit"
-            >
-            </IconButton>
+            </div>
           </Box>
         </Toolbar>
       </AppBar>
